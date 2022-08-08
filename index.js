7b086197ee74e156609539d7e7f861988afdb003
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-08-08 16:45:33 -0400

    both dice are generating random numbers and dice picture reflects value

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-var randomNumber1 = Math.random();
-randomNumber1 = randomNumber1 * 6;
-randomNumber1 = Math.floor(randomNumber1) + 1;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1 - 6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png 
+
+var randomImageSource = "images/" + randomDiceImage; // images/dice1.png = images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
