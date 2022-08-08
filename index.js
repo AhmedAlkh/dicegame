4090a5c71a2b507d03f6593bdd2471dcb6d7cf33
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-08-08 16:50:43 -0400

    page title is altered to display winner of the dice roll

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+// if Player 1 wins
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins!"
+} 
+// if Player 2 wins
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 wins!"
+}
+// if its a draw
+else {
+    document.querySelector("h1").innerHTML = "Draw!"
+}
